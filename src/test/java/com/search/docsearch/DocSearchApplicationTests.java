e39103a115bbd3b8186e1a18e05a15dc1beed7a4
陈阳 <775517776@qq.com>
2022-10-28 15:46:35 +0800

    “顶顶顶顶”

--- src/test/java/com/search/docsearch/DocSearchApplicationTests.java
+++ src/test/java/com/search/docsearch/DocSearchApplicationTests.java
@@ -15,10 +15,14 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.nio.conn.ssl.SSLIOSessionStrategy;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.TrustStrategy;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.ClearScrollRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
@@ -28,9 +32,12 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.TermQueryBuilder;
 import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.index.reindex.DeleteByQueryRequest;
+import org.elasticsearch.search.Scroll;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.suggest.Suggest;
 import org.elasticsearch.search.suggest.SuggestBuilder;
@@ -134,13 +141,78 @@ class DocSearchApplicationTests {
 
 	@Test
 	void testMigrate() throws IOException {
+		System.out.println("begin --------");
+		long st = System.currentTimeMillis();
 		//源es
 		RestHighLevelClient input = getEsClientSecurity("host", 9200, "username", "password");
 		//目标迁移es
 		RestHighLevelClient output = getEsClientSecurity("host", 9200, "username", "password");
+//		RestHighLevelClient output = getEsClient("127.0.0.1", 9200);
+		//你需要迁移的index
+		String index = "mindspore_articles";
 
+		int scrollSize = 500;//一次读取的doc数量
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+		searchSourceBuilder.query(QueryBuilders.matchAllQuery());//读取全量数据
+		searchSourceBuilder.size(scrollSize);
+		Scroll scroll = new Scroll(TimeValue.timeValueMinutes(10));//设置一次读取的最大连接时长
 
+		SearchRequest searchRequest = new SearchRequest(index);
+//        searchRequest1.types("_doc");
+		searchRequest.source(searchSourceBuilder);
+		searchRequest.scroll(scroll);
 
+		SearchResponse searchResponse = input.search(searchRequest, RequestOptions.DEFAULT);
+
+		String scrollId = searchResponse.getScrollId();
+		System.out.println("scrollId - " + scrollId);
+
+		SearchHit[] hits = searchResponse.getHits().getHits();
+		System.out.println("hits - " + hits.length);
+		BulkRequest bulkRequest = new BulkRequest();
+		for (SearchHit hit : hits) {
+			IndexRequest indexRequest = new IndexRequest(index).source(hit.getSourceAsMap());
+
+			bulkRequest.add(indexRequest);
+		}
+		if (bulkRequest.requests().size() > 0) {
+			BulkResponse q = output.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+			System.out.println("wrong ? " + q.hasFailures());
+		}
+
+		while (hits.length > 0) {
+			SearchScrollRequest searchScrollRequestS = new SearchScrollRequest(scrollId);
+			searchScrollRequestS.scroll(scroll);
+			SearchResponse searchScrollResponseS = input.scroll(searchScrollRequestS,RequestOptions.DEFAULT);
+			scrollId = searchScrollResponseS.getScrollId();
+			System.out.println("scrollId - " + scrollId);
+
+			hits = searchScrollResponseS.getHits().getHits();
+			System.out.println("hits - " + hits.length);
+
+			BulkRequest bulkRequestS = new BulkRequest();
+			for (SearchHit hit : hits) {
+				IndexRequest indexRequest = new IndexRequest(index).source(hit.getSourceAsMap());
+
+				bulkRequestS.add(indexRequest);
+			}
+			if (bulkRequestS.requests().size() > 0) {
+				BulkResponse q = output.bulk(bulkRequestS, RequestOptions.DEFAULT);
+
+				System.out.println("wrong ? " + q.hasFailures());
+			}
+
+		}
+
+		ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+		clearScrollRequest.addScrollId(scrollId);
+		try {
+			input.clearScroll(clearScrollRequest,RequestOptions.DEFAULT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("读取用时:" + (System.currentTimeMillis() - st));
 
 	}
 
@@ -160,7 +232,7 @@ class DocSearchApplicationTests {
 		RestHighLevelClient restClient = null;
 		try {
 			final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-			credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("admin", "Cloudfoundry@123"));
+			credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 			SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
 				@Override
 				public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
