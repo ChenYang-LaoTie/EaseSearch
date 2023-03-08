3c1356c189ca2728260c7546ba8507509b055a73
陈阳 <775517776@qq.com>
2023-03-08 16:02:00 +0800

    .

--- src/main/java/com/search/docsearch/parse/MINDSPORE.java
+++ src/main/java/com/search/docsearch/parse/MINDSPORE.java
@@ -176,10 +176,6 @@ public class MINDSPORE {
             String url = System.getenv("murl");
             String username = System.getenv("musername");
             String password = System.getenv("mpassword");
-            url = "jdbc:mysql://192.168.1.203:3306/website?useUnicode=true&characterEncoding=utf-8&useSSL=false";
-            username = "root";
-            password = "root";
-
 
             conn = DriverManager.getConnection(url,username,password);
 
