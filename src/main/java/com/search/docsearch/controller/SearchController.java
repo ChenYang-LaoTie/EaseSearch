a80444be01541a4e823ab4cea69cfe56290be54e
陈阳 <775517776@qq.com>
2023-03-20 14:14:15 +0800

    .

--- src/main/java/com/search/docsearch/controller/SearchController.java
+++ src/main/java/com/search/docsearch/controller/SearchController.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -21,6 +18,8 @@ import java.util.Map;
 @RestController
 @Slf4j
 @RequestMapping("/search")
+
+@CrossOrigin(origins = "*")
 public class SearchController {
 
     @Autowired
