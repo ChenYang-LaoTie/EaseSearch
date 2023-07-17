faa311cd1cd934ce0719212ca391b1a8fe6d5188
陈阳 <775517776@qq.com>
2023-07-17 19:02:22 +0800

    afd

--- src/main/java/com/search/docsearch/config/InitConfigValue.java
+++ src/main/java/com/search/docsearch/config/InitConfigValue.java
@@ -2,6 +2,7 @@ package com.search.docsearch.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ public class InitConfigValue {
     @Value("${config-path}")
     private String configPath;
 
+    @Bean
     public void deletEvnformApplication() {
         // if (FileUtils.deleteFile(configPath)) {
         //     log.info("delete application success");
