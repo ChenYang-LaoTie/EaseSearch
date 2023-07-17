a78c3279927baa626fa7b433b7ea6b19ebdcc26e
陈阳 <775517776@qq.com>
2023-07-17 18:46:31 +0800

    dfadsf

--- src/main/java/com/search/docsearch/config/InitConfigValue.java
+++ src/main/java/com/search/docsearch/config/InitConfigValue.java
@@ -1,12 +1,14 @@
 package com.search.docsearch.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+@EnableConfigurationProperties(InitConfigValue.class)
 public class InitConfigValue {
 
     @Value("${config-path}")
