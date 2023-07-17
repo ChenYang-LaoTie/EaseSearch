cecf0685a4377745f673a03196ef214f5ae44baf
陈阳 <775517776@qq.com>
2023-07-17 18:58:42 +0800

    fdsaf

--- src/main/java/com/search/docsearch/config/ElasticSearchConfig.java
+++ src/main/java/com/search/docsearch/config/ElasticSearchConfig.java
@@ -27,7 +27,7 @@ import org.springframework.context.annotation.Configuration;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
-@EnableConfigurationProperties(ElasticSearchConfig.class)
+@EnableConfigurationProperties()
 @Slf4j
 public class ElasticSearchConfig {
 
--- src/main/java/com/search/docsearch/config/InitConfigValue.java
+++ src/main/java/com/search/docsearch/config/InitConfigValue.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableConfigurationProperties(InitConfigValue.class)
+@EnableConfigurationProperties()
 public class InitConfigValue {
 
     @Value("${config-path}")
