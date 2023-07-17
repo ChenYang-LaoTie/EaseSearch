586d483944f083cb3ae2d245d48ed18b87aceb97
陈阳 <775517776@qq.com>
2023-07-17 18:52:47 +0800

    dfsafd

--- src/main/java/com/search/docsearch/config/SystemConfig.java
+++ src/main/java/com/search/docsearch/config/SystemConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
-@EnableConfigurationProperties(MySystem.class)
+@EnableConfigurationProperties(SystemConfig.class)
 @Slf4j
 public class SystemConfig {
 
